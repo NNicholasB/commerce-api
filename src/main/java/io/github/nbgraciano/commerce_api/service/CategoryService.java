7b072c9bfa00b5f01d57225cbb2fb27e0ba1ba0e
Nicholas Bernardo <nicholasbgraciano@gmail.com>
2026-08-10 20:56:31 -0300

    feat: create method

--- src/main/java/io/github/nbgraciano/commerce_api/service/CategoryService.java
+++ src/main/java/io/github/nbgraciano/commerce_api/service/CategoryService.java
@@ -1,5 +1,9 @@
 package io.github.nbgraciano.commerce_api.service;
 
+import io.github.nbgraciano.commerce_api.entity.Category;
+import io.github.nbgraciano.commerce_api.entity.dto.Category.CategoryRequestDTO;
+import io.github.nbgraciano.commerce_api.entity.dto.Category.CategoryResponseDTO;
+import io.github.nbgraciano.commerce_api.entity.mappers.CategoryMapper;
 import io.github.nbgraciano.commerce_api.repository.CategoryRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -10,5 +14,12 @@ public class CategoryService {
 
     private CategoryRepository repository;
 
+    private CategoryMapper mapper;
+
+    public CategoryResponseDTO create(CategoryRequestDTO request){
+        Category category=mapper.toEntity(request);
+        Category saved=repository.save(category);
+        return mapper.toResponse(saved);
+    }
 
 }
