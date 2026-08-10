67be12d5fa0c10a4269aecd2aef4d61022b7c31b
Nicholas Bernardo <nicholasbgraciano@gmail.com>
2026-08-10 20:55:33 -0300

    feat: add DTO validation

--- src/main/java/io/github/nbgraciano/commerce_api/entity/dto/Order/OrderRequestDTO.java
+++ src/main/java/io/github/nbgraciano/commerce_api/entity/dto/Order/OrderRequestDTO.java
@@ -2,13 +2,14 @@ package io.github.nbgraciano.commerce_api.entity.dto.Order;
 
 import io.github.nbgraciano.commerce_api.entity.Status;
 import io.github.nbgraciano.commerce_api.entity.Users;
+import jakarta.validation.constraints.NotBlank;
 
 import java.math.BigDecimal;
 import java.util.List;
 import java.util.UUID;
 
 public record OrderRequestDTO(
-
+        @NotBlank(message = "User obligatory")
         Users usersId,
         List items
 
