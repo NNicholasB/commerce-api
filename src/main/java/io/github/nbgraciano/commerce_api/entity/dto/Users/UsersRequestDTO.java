104e5348d9364ff39a95ed84ac3f394378fb2937
Nicholas Bernardo <nicholasbgraciano@gmail.com>
2026-08-10 20:56:00 -0300

    feat: add DTO validation

--- src/main/java/io/github/nbgraciano/commerce_api/entity/dto/Users/UsersRequestDTO.java
+++ src/main/java/io/github/nbgraciano/commerce_api/entity/dto/Users/UsersRequestDTO.java
@@ -1,8 +1,20 @@
 package io.github.nbgraciano.commerce_api.entity.dto.Users;
 
+import jakarta.persistence.Entity;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
 public record UsersRequestDTO(
+
+        @NotBlank(message = "Name is obligatory")
+        @Size(max = 100,message = "The name must be no more than 100 characters")
         String name,
+        @NotBlank(message = "Password is obligatory")
+        @Size(min = 8, max = 100, message = "The password must be between 8 and 100 characters")
         String password,
-        String email,
-        String role
+        @NotBlank(message = "Email is required")
+        @Email(message = "invalid email")
+        String email
+
 ) {}
