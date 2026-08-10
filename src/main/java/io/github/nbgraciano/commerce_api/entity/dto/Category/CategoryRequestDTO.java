700cfdf330435d7da3dbd07772d7e163f7d260fd
Nicholas Bernardo <nicholasbgraciano@gmail.com>
2026-08-10 20:55:21 -0300

    feat: add DTO validation

--- src/main/java/io/github/nbgraciano/commerce_api/entity/dto/Category/CategoryRequestDTO.java
+++ src/main/java/io/github/nbgraciano/commerce_api/entity/dto/Category/CategoryRequestDTO.java
@@ -1,6 +1,12 @@
 package io.github.nbgraciano.commerce_api.entity.dto.Category;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
 public record CategoryRequestDTO(
+
+        @NotBlank(message = "Name obligatory")
+        @Size(min=2,max=100,message = "The name must be no more than 100 characters")
         String name
 ) {
 }
