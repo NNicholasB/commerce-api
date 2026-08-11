0c87fb07fcecd13f78c9e77d8040e0aa7ff872ea
Nicholas Bernardo <nicholasbgraciano@gmail.com>
2026-08-11 19:53:23 -0300

    feat:add some valids

--- src/main/java/io/github/nbgraciano/commerce_api/entity/dto/OrderItem/OrderItemRequestDTO.java
+++ src/main/java/io/github/nbgraciano/commerce_api/entity/dto/OrderItem/OrderItemRequestDTO.java
@@ -1,14 +1,19 @@
 package io.github.nbgraciano.commerce_api.entity.dto.OrderItem;
 
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
 import jakarta.validation.constraints.Size;
 
 import java.util.UUID;
 
 public record OrderItemRequestDTO(
-        @NotBlank(message = "Id obligatory")
+
+        @NotNull(message = "Product is required")
         UUID productId,
-        @Size(min = 1,message = "The quantity must be greater than 0")
+
+        @NotNull(message = "Quantity is required")
+        @Positive(message = "Quantity must be greater than zero")
         Integer quantity
 
 ) {}
