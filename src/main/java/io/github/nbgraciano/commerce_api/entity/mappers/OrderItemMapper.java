f5b6961e51caaa3994d5cd7c6bea5d3920313bc0
Nicholas Bernardo <nicholasbgraciano@gmail.com>
2026-08-11 19:54:38 -0300

    feat: add List toResponse on OrderItemMapper

--- src/main/java/io/github/nbgraciano/commerce_api/entity/mappers/OrderItemMapper.java
+++ src/main/java/io/github/nbgraciano/commerce_api/entity/mappers/OrderItemMapper.java
@@ -5,10 +5,14 @@ import io.github.nbgraciano.commerce_api.entity.dto.OrderItem.OrderItemRequestDT
 import io.github.nbgraciano.commerce_api.entity.dto.OrderItem.OrderItemResponseDTO;
 import org.mapstruct.Mapper;
 
+import java.util.List;
+
 @Mapper(componentModel = "spring")
 public interface OrderItemMapper {
 
     OrderItemResponseDTO toResponse(OrderItem orderItem);
+
+    List<OrderItemResponseDTO> toResponse(List<OrderItem> list);
 
     OrderItem toEntity(OrderItemRequestDTO request);
 }
