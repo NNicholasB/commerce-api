2e6feec1f53bf580a25d61bcb3e763fccf026fd2
Nicholas Bernardo <nicholasbgraciano@gmail.com>
2026-08-11 19:54:15 -0300

    feat: add List toResponse on CategoryMapper

--- src/main/java/io/github/nbgraciano/commerce_api/entity/mappers/CategoryMapper.java
+++ src/main/java/io/github/nbgraciano/commerce_api/entity/mappers/CategoryMapper.java
@@ -5,10 +5,14 @@ import io.github.nbgraciano.commerce_api.entity.dto.Category.CategoryRequestDTO;
 import io.github.nbgraciano.commerce_api.entity.dto.Category.CategoryResponseDTO;
 import org.mapstruct.Mapper;
 
+import java.util.List;
+
 @Mapper(componentModel = "spring")
 public interface CategoryMapper {
 
     CategoryResponseDTO toResponse(Category category);
+
+    List<CategoryResponseDTO> toResponse(List<Category> list);
 
     Category toEntity(CategoryRequestDTO request);
 }
