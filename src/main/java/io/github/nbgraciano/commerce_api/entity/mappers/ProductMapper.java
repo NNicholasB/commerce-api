f365a36e63290c3d581a19d76e5afc46203d4bf5
Nicholas Bernardo <nicholasbgraciano@gmail.com>
2026-08-11 19:55:26 -0300

    feat: add List toResponse on ProductMapper

--- src/main/java/io/github/nbgraciano/commerce_api/entity/mappers/ProductMapper.java
+++ src/main/java/io/github/nbgraciano/commerce_api/entity/mappers/ProductMapper.java
@@ -1,14 +1,21 @@
 package io.github.nbgraciano.commerce_api.entity.mappers;
 
+import io.github.nbgraciano.commerce_api.entity.Order;
 import io.github.nbgraciano.commerce_api.entity.Product;
+import io.github.nbgraciano.commerce_api.entity.dto.Order.OrderResponseDTO;
 import io.github.nbgraciano.commerce_api.entity.dto.Product.ProductRequestDTO;
 import io.github.nbgraciano.commerce_api.entity.dto.Product.ProductResponseDTO;
 import org.mapstruct.Mapper;
+
+import java.util.List;
 
 @Mapper(componentModel = "spring")
 public interface ProductMapper {
 
     ProductResponseDTO toResponse(Product product);
+
+    List<ProductResponseDTO> toResponse(List<Product> list);
+
 
     Product toEntity(ProductRequestDTO request);
 }
