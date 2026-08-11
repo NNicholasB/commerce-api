c0a06b7fbd7d93c2aea1940fdc60e5be34018414
Nicholas Bernardo <nicholasbgraciano@gmail.com>
2026-08-11 19:57:21 -0300

    fix: rename file

--- src/main/java/io/github/nbgraciano/commerce_api/repository/UsersRepository.java
+++ src/main/java/io/github/nbgraciano/commerce_api/repository/UsersRepository.java
@@ -4,5 +4,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.UUID;
 
-public interface Users extends JpaRepository<Users, UUID> {
+public interface UsersRepository extends JpaRepository<UsersRepository, UUID> {
 }
