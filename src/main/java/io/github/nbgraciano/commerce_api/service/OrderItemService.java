358122f6704dc3b908d807bb5b2c160378cbefa5
Nicholas Bernardo <nicholasbgraciano@gmail.com>
2026-08-13 20:29:14 -0300

    feat: methods create, findbyid, findall, delete and update

--- src/main/java/io/github/nbgraciano/commerce_api/service/OrderItemService.java
+++ src/main/java/io/github/nbgraciano/commerce_api/service/OrderItemService.java
@@ -1,6 +1,7 @@
 package io.github.nbgraciano.commerce_api.service;
 
 
+import io.github.nbgraciano.commerce_api.entity.Order;
 import io.github.nbgraciano.commerce_api.entity.OrderItem;
 import io.github.nbgraciano.commerce_api.entity.Product;
 import io.github.nbgraciano.commerce_api.entity.dto.OrderItem.OrderItemRequestDTO;
@@ -8,11 +9,15 @@ import io.github.nbgraciano.commerce_api.entity.dto.OrderItem.OrderItemResponseD
 import io.github.nbgraciano.commerce_api.entity.mappers.OrderItemMapper;
 import io.github.nbgraciano.commerce_api.exception.EntityNotFoundException;
 import io.github.nbgraciano.commerce_api.repository.OrderItemRepository;
+import io.github.nbgraciano.commerce_api.repository.OrderRepository;
 import io.github.nbgraciano.commerce_api.repository.ProductRepository;
+import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import java.math.BigDecimal;
+import java.util.List;
+import java.util.UUID;
 
 @Service
 @RequiredArgsConstructor
@@ -21,23 +26,82 @@ public class OrderItemService {
     private final OrderItemRepository repository;
     private final ProductRepository productRepository;
     private final OrderItemMapper mapper;
+    private final OrderService orderService;
 
-    public OrderItemResponseDTO create(OrderItemRequestDTO request){
+    @Transactional
+    public OrderItemResponseDTO create(UUID orderId,OrderItemRequestDTO request){
             Product product=productRepository.findById(request.productId()).orElseThrow(()->
                     new EntityNotFoundException("Product not found"));
 
+        Order order = orderService.findEntityById(orderId);
+
         OrderItem orderItem=new OrderItem();
-        orderItem.setId(product.getId());
+
         orderItem.setProduct(product);
         orderItem.setQuantity(request.quantity());
         orderItem.setUnitPrice(product.getPrice());
+        orderItem.setOrder(order);
         BigDecimal subtotal = product.getPrice()
                 .multiply(BigDecimal.valueOf(request.quantity()));
         orderItem.setSubtotal(subtotal);
 
         OrderItem saved=repository.save(orderItem);
+        orderService.recalculateTotal(order);
         return mapper.toResponse(saved);
 
     }
+
+    public OrderItemResponseDTO findById(UUID id){
+        OrderItem orderItem= repository.findById(id).orElseThrow(()->
+                new EntityNotFoundException("OrderItem not found"));
+
+        return mapper.toResponse(orderItem);
+
+    }
+
+    public List<OrderItemResponseDTO> findAll(){
+       return mapper.toResponse(repository.findAll());
+    }
+
+    @Transactional
+    public void deleteById(UUID id){
+        OrderItem orderItem=repository.findById(id).orElseThrow(()->
+                new EntityNotFoundException("OrderItem not found"));
+
+        Order order = orderItem.getOrder();
+
+        order.getItems().remove(orderItem);
+
+        repository.delete(orderItem);
+
+        orderService.recalculateTotal(order);
+    }
+
+    @Transactional
+    public OrderItemResponseDTO update(UUID id,OrderItemRequestDTO request){
+        OrderItem orderItem=repository.findById(id).orElseThrow(()->new EntityNotFoundException("OrderItem not found"));
+        Product product = productRepository.findById(request.productId())
+                .orElseThrow(() ->
+                        new EntityNotFoundException("Product not found")
+                );
+
+        BigDecimal unitPrice = product.getPrice();
+
+        BigDecimal subtotal = unitPrice
+                .multiply(BigDecimal.valueOf(request.quantity()));
+
+        orderItem.setProduct(product);
+        orderItem.setQuantity(request.quantity());
+        orderItem.setSubtotal(subtotal);
+        orderItem.setUnitPrice(unitPrice);
+
+        repository.save(orderItem);
+        orderService.recalculateTotal(orderItem.getOrder());
+        return mapper.toResponse(orderItem);
+
+    }
+
+
+
 
 }
