7221e3dc8eddc30a338eb540c45968252a7966d5
Nicholas Bernardo <nicholasbgraciano@gmail.com>
2026-08-14 19:09:20 -0300

    feat: implementation existsByNameAndCategoryIdAndIdNot and existsByNameAndCategoryId

--- src/main/java/io/github/nbgraciano/commerce_api/repository/ProductRepository.java
+++ src/main/java/io/github/nbgraciano/commerce_api/repository/ProductRepository.java
@@ -1,9 +1,15 @@
 package io.github.nbgraciano.commerce_api.repository;
 
 import io.github.nbgraciano.commerce_api.entity.Product;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.UUID;
 
 public interface ProductRepository extends JpaRepository<Product, UUID> {
+
+
+    boolean existsByNameAndCategoryIdAndIdNot(String name,UUID categoryId,UUID id);
+    boolean existsByNameAndCategoryId(String name, UUID uuid);
 }
