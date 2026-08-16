5cf1ae90b66f6b7759d2d8e7f5369b18b4997868
Nicholas Bernardo <nicholasbgraciano@gmail.com>
2026-08-16 19:47:15 -0300

    fix: rename file

--- src/main/java/io/github/nbgraciano/commerce_api/entity/dto/Password/PasswordChangeDTO.java
+++ src/main/java/io/github/nbgraciano/commerce_api/entity/dto/Password/PasswordChangeDTO.java
@@ -2,7 +2,7 @@ package io.github.nbgraciano.commerce_api.entity.dto.Password;
 
 import jakarta.validation.constraints.NotBlank;
 
-public record PasswordChargeDTO(
+public record PasswordChangeDTO(
 
         @NotBlank(message = "Current password is required")
         String currentPassword,
