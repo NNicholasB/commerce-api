0a1184f19616c2ba4390e11fa0c2a810206b22e6
Nicholas Bernardo <nicholasbgraciano@gmail.com>
2026-08-16 19:37:37 -0300

    fix: fix bugs

--- src/main/java/io/github/nbgraciano/commerce_api/entity/dto/Users/UsersRequestDTO.java
+++ src/main/java/io/github/nbgraciano/commerce_api/entity/dto/Users/UsersRequestDTO.java
@@ -1,6 +1,7 @@
 package io.github.nbgraciano.commerce_api.entity.dto.Users;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.UniqueConstraint;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
@@ -10,9 +11,11 @@ public record UsersRequestDTO(
         @NotBlank(message = "Name is obligatory")
         @Size(max = 100,message = "The name must be no more than 100 characters")
         String name,
+
         @NotBlank(message = "Password is obligatory")
         @Size(min = 8, max = 100, message = "The password must be between 8 and 100 characters")
         String password,
+
         @NotBlank(message = "Email is required")
         @Email(message = "invalid email")
         String email
