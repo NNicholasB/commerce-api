799a0cb7b04fec350d306e95c403835a8fcd57a9
Nicholas Bernardo <nicholasbgraciano@gmail.com>
2026-08-17 21:05:39 -0300

    feat: add method findByEmail

--- src/main/java/io/github/nbgraciano/commerce_api/repository/UsersRepository.java
+++ src/main/java/io/github/nbgraciano/commerce_api/repository/UsersRepository.java
@@ -3,10 +3,12 @@ package io.github.nbgraciano.commerce_api.repository;
 import io.github.nbgraciano.commerce_api.entity.Users;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
 import java.util.UUID;
 
 public interface UsersRepository extends JpaRepository<Users, UUID> {
 
     boolean existsByNameAndEmail(String name,String email);
 
+    Optional<Users> findByEmail(String email);
 }
