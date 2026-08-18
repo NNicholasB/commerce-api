4cb4f6fedf6191aaa5f60a54a8a2812fc483bb5f
Nicholas Bernardo <nicholasbgraciano@gmail.com>
2026-08-17 21:06:14 -0300

    feat: add method Authentication and securityFilterChain

--- src/main/java/io/github/nbgraciano/commerce_api/security/SecurityConfig.java
+++ src/main/java/io/github/nbgraciano/commerce_api/security/SecurityConfig.java
@@ -1,16 +1,45 @@
 package io.github.nbgraciano.commerce_api.security;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
+@EnableMethodSecurity
+@RequiredArgsConstructor
 public class SecurityConfig {
 
     @Bean
-    public SecurityFilterChain
+    public SecurityFilterChain securityFilterChain(HttpSecurity http)throws Exception{
+        http.csrf(csrf->csrf.disable())
+                .sessionManagement(session->
+                        session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .authorizeHttpRequests(auth->auth
+                        .requestMatchers("/auth/**").permitAll()
+                        .requestMatchers(HttpMethod.POST,"/users").permitAll()
+                        .requestMatchers("/admin/**").hasRole("ADMIN")
+
+                        .anyRequest()
+                        .authenticated()
+                );
+        return http.build();
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(
+            AuthenticationConfiguration configuration)throws Exception{
+        return configuration.getAuthenticationManager();
+    }
+
 
     @Bean
     public PasswordEncoder passwordEncoder(){
