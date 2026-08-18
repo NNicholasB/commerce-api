07573001b92e4418e82b7179217a0c544902dd95
Nicholas Bernardo <nicholasbgraciano@gmail.com>
2026-08-17 21:07:51 -0300

    feat: CustomUsertDetailsService method

---
+++ src/main/java/io/github/nbgraciano/commerce_api/security/CustomUserDetailsService.java
@@ -0,0 +1,21 @@
+package io.github.nbgraciano.commerce_api.security;
+
+import io.github.nbgraciano.commerce_api.entity.Users;
+import io.github.nbgraciano.commerce_api.repository.UsersRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+
+
+@RequiredArgsConstructor
+public class CustomUserDetailsService implements UserDetailsService {
+
+    private final UsersRepository repository;
+
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException{
+        Users user=repository.findByEmail(email).orElseThrow(()-> new UsernameNotFoundException("User not found"));
+
+        return new CustomUserDetails(user);
+    }
+}
