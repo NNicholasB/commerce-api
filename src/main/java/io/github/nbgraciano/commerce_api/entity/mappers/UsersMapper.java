3fa9e60c4e61266d52ddd5b86524839c3f2f5a5d
Nicholas Bernardo <nicholasbgraciano@gmail.com>
2026-08-19 20:30:22 -0300

    feat: add mapper config

--- src/main/java/io/github/nbgraciano/commerce_api/entity/mappers/UsersMapper.java
+++ src/main/java/io/github/nbgraciano/commerce_api/entity/mappers/UsersMapper.java
@@ -5,6 +5,7 @@ import io.github.nbgraciano.commerce_api.entity.Users;
 import io.github.nbgraciano.commerce_api.entity.dto.Users.UsersRequestDTO;
 import io.github.nbgraciano.commerce_api.entity.dto.Users.UsersResponseDTO;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 
 import java.util.List;
 
@@ -15,5 +16,7 @@ public interface UsersMapper {
 
     List<UsersResponseDTO> toResponse(List<Users> list);
 
+    @Mapping(target = "id", ignore = true)
+    @Mapping(target = "role", ignore = true)
     Users toEntity(UsersRequestDTO request);
 }
