545e3c3ae914b4611243ed84c1eaaae99fd7e6ee
Nicholas Bernardo <nicholasbgraciano@gmail.com>
2026-08-19 20:30:49 -0300

    feat: implementation loginRepsonseDTO

--- src/main/java/io/github/nbgraciano/commerce_api/security/AuthenticationService.java
+++ src/main/java/io/github/nbgraciano/commerce_api/security/AuthenticationService.java
@@ -1,8 +1,12 @@
 package io.github.nbgraciano.commerce_api.security;
 
 
+import io.github.nbgraciano.commerce_api.entity.dto.login.LoginRequestDTO;
+import io.github.nbgraciano.commerce_api.entity.dto.login.LoginResponseDTO;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -11,5 +15,14 @@ public class AuthenticationService {
 
 
     private final AuthenticationManager authenticationManager;
-    private final JwtService
+    private final JwtService jwtService;
+
+    public LoginResponseDTO login(LoginRequestDTO request){
+        Authentication authentication= authenticationManager.authenticate(
+                new UsernamePasswordAuthenticationToken(request.email(),request.password())
+        );
+        String token=jwtService.generateToken(authentication);
+        return new LoginResponseDTO(token);
+    }
+
 }
