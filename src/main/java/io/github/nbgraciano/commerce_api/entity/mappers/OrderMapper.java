0fec272bea5e732077e13d173e62e6edb8a4c98f
Nicholas Bernardo <nicholasbgraciano@gmail.com>
2026-08-19 20:30:03 -0300

    feat: add mapper config

--- src/main/java/io/github/nbgraciano/commerce_api/entity/mappers/OrderMapper.java
+++ src/main/java/io/github/nbgraciano/commerce_api/entity/mappers/OrderMapper.java
@@ -5,17 +5,22 @@ import io.github.nbgraciano.commerce_api.entity.Order;
 import io.github.nbgraciano.commerce_api.entity.dto.Order.OrderRequestDTO;
 import io.github.nbgraciano.commerce_api.entity.dto.Order.OrderResponseDTO;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 
 import java.util.List;
 
-@Mapper(componentModel = "spring")
+@Mapper(componentModel = "spring",  uses = OrderItemMapper.class)
 public interface OrderMapper {
 
+    @Mapping(target = "userId", source = "user.id")
     OrderResponseDTO toResponse(Order order);
 
     List<OrderResponseDTO> toResponse(List<Order> list);
 
-
+    @Mapping(target = "id", ignore = true)
+    @Mapping(target = "user", ignore = true)
+    @Mapping(target = "status", ignore = true)
+    @Mapping(target = "total", ignore = true)
     Order toEntity(OrderRequestDTO request);
 
 
