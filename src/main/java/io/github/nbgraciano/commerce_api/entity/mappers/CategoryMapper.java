cc94e071000447a7bb86ee158e6f22951ed79d36
Nicholas Bernardo <nicholasbgraciano@gmail.com>
2026-08-20 19:53:41 -0300

    feat: add Mapping target id

--- src/main/java/io/github/nbgraciano/commerce_api/entity/mappers/CategoryMapper.java
+++ src/main/java/io/github/nbgraciano/commerce_api/entity/mappers/CategoryMapper.java
@@ -4,6 +4,7 @@ import io.github.nbgraciano.commerce_api.entity.Category;
 import io.github.nbgraciano.commerce_api.entity.dto.Category.CategoryRequestDTO;
 import io.github.nbgraciano.commerce_api.entity.dto.Category.CategoryResponseDTO;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 
 import java.util.List;
 
@@ -14,5 +15,6 @@ public interface CategoryMapper {
 
     List<CategoryResponseDTO> toResponse(List<Category> list);
 
+    @Mapping(target = "id", ignore = true)
     Category toEntity(CategoryRequestDTO request);
 }
