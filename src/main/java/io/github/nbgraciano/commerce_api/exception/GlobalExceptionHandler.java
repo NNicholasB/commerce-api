103ffa561cce19f5cc4a6bcde9e0c4106ef9f144
Nicholas Bernardo <nicholasbgraciano@gmail.com>
2026-08-20 19:54:47 -0300

    feat: add HandleValidationException for body in json

--- src/main/java/io/github/nbgraciano/commerce_api/exception/GlobalExceptionHandler.java
+++ src/main/java/io/github/nbgraciano/commerce_api/exception/GlobalExceptionHandler.java
@@ -2,8 +2,13 @@ package io.github.nbgraciano.commerce_api.exception;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.awt.image.ReplicateScaleFilter;
+import java.util.HashMap;
+import java.util.Map;
 
 @RestControllerAdvice
 public class GlobalExceptionHandler {
@@ -47,4 +52,14 @@ public class GlobalExceptionHandler {
     public ResponseEntity<ErrorResponse> handlePassword(InvalidPasswordException ex){
         return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(new ErrorResponse(400,ex.getMessage()));
     }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<Map<String,String>> handleValidationException(MethodArgumentNotValidException ex){
+        Map<String,String> errors=new HashMap<>();
+
+        ex.getBindingResult().getFieldErrors().forEach(er->errors.put(er.getField(),er.getDefaultMessage()));
+
+        return  ResponseEntity.status(HttpStatus.BAD_REQUEST).body(errors);
+    }
+
 }
