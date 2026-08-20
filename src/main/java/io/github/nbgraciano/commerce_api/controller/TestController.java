8d9cf11ef6e1fd67169cfee26f3ef7a1283abce5
Nicholas Bernardo <nicholasbgraciano@gmail.com>
2026-08-20 19:54:09 -0300

    feat: add testController for testAuth

---
+++ src/main/java/io/github/nbgraciano/commerce_api/controller/TestController.java
@@ -0,0 +1,22 @@
+package io.github.nbgraciano.commerce_api.controller;
+
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/test")
+public class TestController {
+
+    @GetMapping("/public")
+    public  String publicRoute(){
+        return "Rota publica";
+    }
+
+    @GetMapping("/private")
+    public  String privateRoute(){
+        return "Rota privada";
+    }
+
+}
