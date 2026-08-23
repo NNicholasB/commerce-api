6e9d1de105762cb05abf516547a1f0e9dbe7c596
Nicholas Bernardo <nicholasbgraciano@gmail.com>
2026-08-22 21:21:40 -0300

    feat: usersController v2

--- src/main/java/io/github/nbgraciano/commerce_api/controller/UsersController.java
+++ src/main/java/io/github/nbgraciano/commerce_api/controller/UsersController.java
@@ -1,6 +1,8 @@
 package io.github.nbgraciano.commerce_api.controller;
 
 
+import io.github.nbgraciano.commerce_api.entity.dto.Password.PasswordChangeDTO;
+import io.github.nbgraciano.commerce_api.entity.dto.Role.ChangeRoleDTO;
 import io.github.nbgraciano.commerce_api.entity.dto.Users.UsersRequestDTO;
 import io.github.nbgraciano.commerce_api.entity.dto.Users.UsersResponseDTO;
 import io.github.nbgraciano.commerce_api.service.UsersService;
@@ -8,20 +10,66 @@ import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import java.net.URI;
+import java.util.List;
+import java.util.UUID;
 
 @RequiredArgsConstructor
 @RestController
-@RequestMapping("/user")
+@RequestMapping("/users")
 public class UsersController {
 
     private final UsersService service;
 
     @PostMapping("/register")
     public ResponseEntity<UsersResponseDTO> register(@Valid @RequestBody UsersRequestDTO request){
-        return ResponseEntity.status(HttpStatus.CREATED).body(service.create(request));
+        UsersResponseDTO response = service.create(request);
+        URI uri= ServletUriComponentsBuilder.fromCurrentRequest()
+                .path("/{id}")
+                .buildAndExpand(response.id())
+                .toUri();
+        return ResponseEntity.created(uri).body(response);
+    }
+
+    @GetMapping()
+    public ResponseEntity<List<UsersResponseDTO>> findAll(@RequestParam(required = false) String name,@RequestParam(required = false)String email){
+        List<UsersResponseDTO> lista= service.findAll(name, email);
+        return ResponseEntity.ok().body(lista);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<UsersResponseDTO> findById(@PathVariable UUID id){
+        return ResponseEntity.ok().body(service.findById(id));
+    }
+
+    @DeleteMapping("{id}")
+    public void deleteById(@Valid @PathVariable UUID id){
+        service.delete(id);
+
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<UsersResponseDTO> update(@PathVariable UUID id,@Valid @RequestBody UsersRequestDTO request){
+        UsersResponseDTO response=service.update(id,request);
+        URI uri= ServletUriComponentsBuilder.fromCurrentRequest()
+                .path("/{id}")
+                .buildAndExpand(response.id())
+                .toUri();
+        return ResponseEntity.created(uri).body(response);
+    }
+
+    @PatchMapping("/{id}/password")
+    public ResponseEntity<Void> chargePassword(@PathVariable UUID id, @Valid @RequestBody PasswordChangeDTO request){
+        service.changePassword(id,request);
+        return  ResponseEntity.noContent().build();
+    }
+
+    @PatchMapping("/{id}/role")
+    public ResponseEntity<Void> chargeRole(@PathVariable UUID id,@Valid @RequestBody ChangeRoleDTO request){
+        service.changeRole(id,request);
+        return ResponseEntity.noContent().build();
     }
 }
