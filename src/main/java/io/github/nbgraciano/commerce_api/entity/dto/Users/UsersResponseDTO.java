63da8f071a7f929bae9fec64e363f7c1a04598fa
Nicholas Bernardo <nicholasbgraciano@gmail.com>
2026-08-22 21:21:21 -0300

    feat:add UUID id

--- src/main/java/io/github/nbgraciano/commerce_api/entity/dto/Users/UsersResponseDTO.java
+++ src/main/java/io/github/nbgraciano/commerce_api/entity/dto/Users/UsersResponseDTO.java
@@ -1,6 +1,9 @@
 package io.github.nbgraciano.commerce_api.entity.dto.Users;
 
+import java.util.UUID;
+
 public record UsersResponseDTO(
+        UUID id,
         String name,
         String email,
         String role
