7b91b3abbe76e856d8603116a8cac123a77bf7b9
Nicholas Bernardo <nicholasbgraciano@gmail.com>
2026-08-22 21:22:02 -0300

    feat:add findByNameContainingIgnoreCase

--- src/main/java/io/github/nbgraciano/commerce_api/repository/CategoryRepository.java
+++ src/main/java/io/github/nbgraciano/commerce_api/repository/CategoryRepository.java
@@ -3,10 +3,12 @@ package io.github.nbgraciano.commerce_api.repository;
 import io.github.nbgraciano.commerce_api.entity.Category;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.UUID;
 
 public interface CategoryRepository extends JpaRepository<Category,UUID> {
 
     boolean existsByName(String name);
     boolean existsByNameAndIdNot(String name, UUID id);
+    List<Category> findByNameContainingIgnoreCase(String name);
 }
