38f37646291b1a5c7792098f66eaa16531ded8db
Nicholas Bernardo <nicholasbgraciano@gmail.com>
2026-08-22 21:22:24 -0300

    feat:add findByNameContainingIgnoreCase and findByEmailContainingIgnoreCase

--- src/main/java/io/github/nbgraciano/commerce_api/repository/UsersRepository.java
+++ src/main/java/io/github/nbgraciano/commerce_api/repository/UsersRepository.java
@@ -3,12 +3,17 @@ package io.github.nbgraciano.commerce_api.repository;
 import io.github.nbgraciano.commerce_api.entity.Users;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
 public interface UsersRepository extends JpaRepository<Users, UUID> {
 
     boolean existsByNameAndEmail(String name,String email);
+
+    List<Users> findByNameContainingIgnoreCase(String name);
+
+    List<Users> findByEmailContainingIgnoreCase(String email);
 
     Optional<Users> findByEmail(String email);
 }
