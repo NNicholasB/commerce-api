c275e0cf224e19a37c35ec6031ee9c43dce1e376
Nicholas Bernardo <nicholasbgraciano@gmail.com>
2026-08-24 20:59:45 -0300

    fix: fix bugs

--- src/main/java/io/github/nbgraciano/commerce_api/entity/dto/Product/ProductRequestDTO.java
+++ src/main/java/io/github/nbgraciano/commerce_api/entity/dto/Product/ProductRequestDTO.java
@@ -1,7 +1,6 @@
 package io.github.nbgraciano.commerce_api.entity.dto.Product;
 
-import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.Size;
+import jakarta.validation.constraints.*;
 
 import java.math.BigDecimal;
 import java.util.UUID;
@@ -14,10 +13,14 @@ public record ProductRequestDTO(
         @NotBlank(message = "Description obligatory")
         @Size(max = 150,message = "The name must be no more than 150 characters")
         String description,
-        @NotBlank(message = "Price obligatory")
+
+        @NotNull(message = "Price obligatory")
+        @Positive
         BigDecimal price,
-        @NotBlank(message = "Stock obligatory")
+
+        @NotNull(message = "Stock obligatory")
+        @PositiveOrZero
         Integer stock,
-        @NotBlank(message = "Category obligatory")
+       @NotNull(message = "Category obligatory")
         UUID categoryId
 ) {}
