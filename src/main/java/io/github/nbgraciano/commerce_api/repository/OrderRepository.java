178e68c39955afd6ad70aca2e3bca4523c133dac
Nicholas Bernardo <nicholasbgraciano@gmail.com>
2026-08-23 21:47:39 -0300

    fix: fix bug

--- src/main/java/io/github/nbgraciano/commerce_api/repository/OrderRepository.java
+++ src/main/java/io/github/nbgraciano/commerce_api/repository/OrderRepository.java
@@ -1,11 +1,11 @@
 package io.github.nbgraciano.commerce_api.repository;
 
+
+
 import io.github.nbgraciano.commerce_api.entity.Order;
-import io.github.nbgraciano.commerce_api.entity.dto.OrderItem.OrderItemRequestDTO;
-import jakarta.validation.Valid;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.List;
+
 import java.util.UUID;
 
 public interface OrderRepository extends JpaRepository<Order, UUID> {
