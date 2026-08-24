f7fc9f1507527a93b4e0f7d9c0b443ae1c31184d
Nicholas Bernardo <nicholasbgraciano@gmail.com>
2026-08-23 21:48:00 -0300

    feat: OrderController v1

---
+++ src/main/java/io/github/nbgraciano/commerce_api/controller/OrderController.java
@@ -0,0 +1,54 @@
+package io.github.nbgraciano.commerce_api.controller;
+
+
+import io.github.nbgraciano.commerce_api.entity.dto.Order.OrderRequestDTO;
+import io.github.nbgraciano.commerce_api.entity.dto.Order.OrderResponseDTO;
+import io.github.nbgraciano.commerce_api.service.OrderService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import java.net.URI;
+import java.util.List;
+import java.util.UUID;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/order")
+public class OrderController {
+
+    private final OrderService service;
+
+    @GetMapping()
+    public ResponseEntity<List<OrderResponseDTO>> findAll(){
+        return ResponseEntity.ok().body(service.findAll());
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<OrderResponseDTO> findById(@PathVariable UUID id){
+        return ResponseEntity.ok().body(service.findById(id));
+    }
+
+    @PostMapping()
+    public ResponseEntity<OrderResponseDTO> create(@RequestBody OrderRequestDTO request){
+        OrderResponseDTO created = service.create(request);
+        URI uri= ServletUriComponentsBuilder.fromCurrentRequest()
+                .path("/{id}")
+                .buildAndExpand(created.id())
+                .toUri();
+        return ResponseEntity.created(uri).body(created);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<OrderResponseDTO> update(@PathVariable UUID id,@RequestBody OrderRequestDTO request){
+        return ResponseEntity.ok().body(service.update(id,request));
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteById(@PathVariable UUID id ){
+        service.deleteById(id);
+        return ResponseEntity.noContent().build();
+    }
+
+}
