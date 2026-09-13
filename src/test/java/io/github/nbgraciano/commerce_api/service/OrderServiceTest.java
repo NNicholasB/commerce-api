4384d73f25994638e363895575ade79d5d07c368
Nicholas Bernardo <nicholasbgraciano@gmail.com>
2026-09-13 16:22:40 -0300

    feat: implemetation cancel method

--- src/test/java/io/github/nbgraciano/commerce_api/service/OrderServiceTest.java
+++ src/test/java/io/github/nbgraciano/commerce_api/service/OrderServiceTest.java
@@ -411,5 +411,55 @@ public class OrderServiceTest {
         verify(mapper,never()).toResponse(any(Order.class));
     }
 
+    @Test
+    @DisplayName("Realizar alteracao no status da Order para cancel")
+    void cancel(){
+
+        UUID orderId = UUID.randomUUID();
+        UUID userId = UUID.randomUUID();
+
+
+        Users user = new Users(
+                userId,
+                "Nicholas",
+                "nic@gmail.com",
+                "123",
+                Role.USER
+        );
+
+        Order order = new Order(orderId,
+                user,
+                Status.WAITING_PAYMENT,
+                new BigDecimal(125),
+                List.of()
+        );
+
+        OrderResponseDTO responseDTO=new OrderResponseDTO(
+                orderId,userId,Status.CANCELED,
+                new BigDecimal(250),
+                List.of());
+
+
+        when(repository.findById(orderId)).thenReturn(Optional.of(order));
+        when(mapper.toResponse(order)).thenReturn(responseDTO);
+        when(repository.save(order)).thenReturn(order);
+
+        OrderResponseDTO result=service.cancel(orderId);
+
+        assertEquals(Status.CANCELED,result.status());
+
+        assertNotNull(result);
+
+        assertEquals(orderId,result.id());
+        assertEquals(userId,result.userId());
+        assertEquals(Status.CANCELED,result.status());
+
+        verify(repository).findById(orderId);
+        verify(repository).save(order);
+        verify(mapper).toResponse(order);
+
+    }
+
+
 
 }
