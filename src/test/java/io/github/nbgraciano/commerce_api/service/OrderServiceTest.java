d7952955de0ac22cf16af4399eb6d8e75b554577
Nicholas Bernardo <nicholasbgraciano@gmail.com>
2026-09-13 20:05:11 -0300

    feat: implemetation erroDeliverStatus Test method

--- src/test/java/io/github/nbgraciano/commerce_api/service/OrderServiceTest.java
+++ src/test/java/io/github/nbgraciano/commerce_api/service/OrderServiceTest.java
@@ -669,5 +669,38 @@ public class OrderServiceTest {
         verify(mapper,never()).toResponse(any(Order.class));
     }
 
+    @Test
+    @DisplayName("Deve lancar excecao status Order nao for WAITING_PAYMENT")
+    void ErroDeliverStatus(){
+
+        UUID orderId = UUID.randomUUID();
+        UUID userId = UUID.randomUUID();
+
+
+        Users user = new Users(
+                userId,
+                "Nicholas",
+                "nic@gmail.com",
+                "123",
+                Role.USER
+        );
+
+        Order order = new Order(orderId,
+                user,
+                Status.DELIVERED,
+                new BigDecimal(125),
+                List.of()
+        );
+
+        when(repository.findById(orderId)).thenReturn(Optional.of(order));
+
+        BusinessException ex = assertThrows(BusinessException.class, () -> service.deliver(orderId));
+
+        assertEquals("Only shipped orders can be delivered",ex.getMessage());
+
+        System.out.println(ex.getMessage());
+        verify(repository,never()).save(any(Order.class));
+        verify(mapper,never()).toResponse(any(Order.class));
+    }
 
 }
